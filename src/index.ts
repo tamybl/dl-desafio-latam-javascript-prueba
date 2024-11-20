c095331cd4b129d82dd091c76ed120008a85b190
Tamara Bravo <tamara.bravo.l@gmail.com>
2024-11-20 03:11:31 -0300

    Ajuste de fecha para new task para correcto ordenamiento

--- src/index.ts
+++ src/index.ts
@@ -33,31 +33,37 @@ class Project implements ProjectI {
         //console.log('Resume Tasks:', resumeTasks);
         return resumeTasks;
     }
-    sortTasks():void {
-        const orderTaks = this.tasks.sort((a, b) => {
+    sortTasks():TaskI[] {
+        const orderTasks = this.tasks.sort((a, b) => {
             const dateA: number = new Date(a.final_date).getTime();
             const dateB: number = new Date(b.final_date).getTime();
-            if(dateA > dateB) {
+            if(dateA < dateB) {
                 return -1;
             }
-            if(dateA < dateB) {
+            if(dateA > dateB) {
                 return 1;
             }
             return  0;
         })
-        console.log('Order by Deadline:', orderTaks);
+        //console.log('Order by Deadline:', orderTasks);
+        return orderTasks;
+        
     }
+}
+
+function filterTaskByProject() {
+
 }
 
 // PARTE I: CASOS DE USO
 // Crear tareas
-const task1 = new Task(1, 'Revisar el diseño del sitio web', 'pending', '22-11-2024');
-const task2 = new Task(2, 'Implementar funcionalidad de búsqueda', 'in progress', '20-11-2024');
-const task3 = new Task(3, 'Corregir errores en formulario de contacto', 'completed', '19-11-2024');
-const task4 = new Task(3, 'Despublicar huincha de información', 'completed', '19-11-2024');
+const task1 = new Task(1, 'Revisar el diseño del sitio web', 'pending', '2024-11-22');
+const task2 = new Task(2, 'Implementar funcionalidad de búsqueda', 'in progress', '2024-11-20');
+const task3 = new Task(3, 'Corregir errores en formulario de contacto', 'completed', '2024-11-19');
+const task4 = new Task(3, 'Despublicar huincha de información', 'completed', '2024-11-19');
 
 // Crear un proyecto
-const project = new Project(101, 'Actualización del sitio web', '15-11-2024');
+const project = new Project(101, 'Actualización del sitio web', '2024-15-11');
 
 // Agregar tareas al proyecto
 project.addTask(task1);
@@ -67,6 +73,11 @@ project.addTask(task4);
 
 // Generar un resumen del estado de las tareas
 const resume = project.resumeProject();
-console.log(resume);
-// PARTE 2
+console.log('Resume Tasks:', resume);
+
+// Ordenar tareas
+const orderTasks = project.sortTasks();
+console.log('Order by Deadline:', orderTasks);
+
+// PARTE II: CASOS DE USO
 
