6b7583fd7182fdfd8df8ef90182e01aca738b43a
Tamara Bravo <tamara.bravo.l@gmail.com>
2024-11-20 04:30:53 -0300

    Termino parte 2 y reordenar casos de uso

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,30 @@ class Project implements ProjectI {
     }
 }
 
+// PARTE I: CASOS DE USO
+// Crear tareas
+const task1 = new Task(1, 'Revisar el diseño del sitio web', 'pending', '2024-11-22');
+const task2 = new Task(2, 'Implementar funcionalidad de búsqueda', 'in progress', '2024-11-20');
+const task3 = new Task(3, 'Corregir errores en formulario de contacto', 'completed', '2024-11-19');
+const task4 = new Task(3, 'Despublicar huincha de información', 'completed', '2024-11-19');
+
+// Crear un proyecto
+const project = new Project(101, 'Actualización del sitio web', '2024-15-11');
+
+// Agregar tareas al proyecto
+project.addTask(task1);
+project.addTask(task2);
+project.addTask(task3);
+project.addTask(task4);
+
+// Generar un resumen del estado de las tareas
+const resume = project.resumeProject();
+console.log('Resume Tasks:', resume);
+
+// Ordenar tareas
+const orderTasks = project.sortTasks();
+console.log('Order by Deadline:', orderTasks);
+
 // PARTE II
 type Operation = (tarea: TaskI) => boolean;
 function filterTaskByProject(project: Project, functionFilter: Operation):TaskI[] {
@@ -79,30 +103,6 @@ function getCriticalTask(project: Project):TaskI[] {
     })
 }
 
-// PARTE I: CASOS DE USO
-// Crear tareas
-const task1 = new Task(1, 'Revisar el diseño del sitio web', 'pending', '2024-11-22');
-const task2 = new Task(2, 'Implementar funcionalidad de búsqueda', 'in progress', '2024-11-20');
-const task3 = new Task(3, 'Corregir errores en formulario de contacto', 'completed', '2024-11-19');
-const task4 = new Task(3, 'Despublicar huincha de información', 'completed', '2024-11-19');
-
-// Crear un proyecto
-const project = new Project(101, 'Actualización del sitio web', '2024-15-11');
-
-// Agregar tareas al proyecto
-project.addTask(task1);
-project.addTask(task2);
-project.addTask(task3);
-project.addTask(task4);
-
-// Generar un resumen del estado de las tareas
-const resume = project.resumeProject();
-console.log('Resume Tasks:', resume);
-
-// Ordenar tareas
-const orderTasks = project.sortTasks();
-console.log('Order by Deadline:', orderTasks);
-
 // PARTE II: CASOS DE USO
 // Filtrar tareas del proyecto
 const today = new Date().toLocaleDateString('en-CA')
@@ -121,4 +121,7 @@ console.log('Days left:', daysLeft);
 // Calcular tareas criticas
 const criticalTasks = getCriticalTask(project);
 console.log('Critical Taks:', criticalTasks);
+
+// PARTE 3
+
 
